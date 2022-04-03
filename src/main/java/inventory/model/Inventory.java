ddbb9d09e964f956134aca08de4f69f59c7c014f
raduamaistroaie <radu.amaistroaie@endava.com>
2022-04-03 17:56:30 +0300

    Added CFG and paths

--- src/main/java/inventory/model/Inventory.java
+++ src/main/java/inventory/model/Inventory.java
@@ -44,15 +44,20 @@ public class Inventory {
      */
     public Product lookupProduct(String searchItem) {
         boolean isFound = false;
+        Product product = null;
         for(Product p: products) {
-            if(p.getName().contains(searchItem) || (p.getProductId()+"").equals(searchItem)) return p;
-            isFound = true;
+            if(p.getName().contains(searchItem) || (p.getProductId()+"").equals(searchItem)) {
+                isFound = true;
+                product = p;
+            }
+            if(isFound == true)
+                return product;
         }
         if(isFound == false) {
             Product product = new Product(0, null, 0.0, 0, 0, 0, null);
             return product;
         }
-        return null;
+        return product;
     }
     
     /**
